bee9f2ccbc6d0146064226fcac6ed6adb6c900ac
Christoph Biernoth <christoph.biernoth@outlook.com>
2024-06-27 14:27:45 +0200

    Restructured code

---
+++ app/clients/mobile/lib/components/screens/player_lobby.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:mobile/components/controls/button.dart';
+import 'package:mobile/components/global/player_avatar.dart';
+import 'package:mobile/state/game_state.dart';
+import 'package:provider/provider.dart';
+
+class PlayerLobby extends StatelessWidget {
+  final VoidCallback onStartGame;
+
+  const PlayerLobby({super.key, required this.onStartGame});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: <Widget>[
+      Padding(
+        padding: const EdgeInsets.only(top: 10, bottom: 40),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '#${Provider.of<GameState>(context, listen: false).game.id}',
+              style: const TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+      Expanded(
+        child: Consumer<GameState>(
+            builder: (context, game, _) => GridView.count(
+                  crossAxisCount: 2,
+                  children: game.game.players
+                      .map((p) => PlayerAvatar(player: p))
+                      .toList(),
+                )),
+      ),
+      Padding(
+        padding: const EdgeInsets.only(top: 40, bottom: 10),
+        child: Button(
+          type: ButtonTypes.accent,
+          title: "Start Game",
+          onPressed: onStartGame,
+        ),
+      )
+    ]);
+  }
+}
--- app/clients/mobile/lib/screens/game_screen.dart
+++ app/clients/mobile/lib/screens/game_screen.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:mobile/components/controls/button.dart';
-import 'package:mobile/components/global/player_avatar.dart';
+import 'package:mobile/components/screens/player_lobby.dart';
 import 'package:mobile/constants/spacing.dart';
-import 'package:mobile/state/game_state.dart';
-import 'package:provider/provider.dart';
 
 class GameScreen extends StatelessWidget {
   const GameScreen({super.key});
@@ -19,37 +16,8 @@ class GameScreen extends StatelessWidget {
         ),
         body: Padding(
             padding: const EdgeInsets.all(Spacing.screenPadding),
-            child: Column(children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(top: 10, bottom: 40),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      '#${Provider.of<GameState>(context, listen: false).game.id}',
-                      style: const TextStyle(
-                          fontSize: 25, fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                ),
-              ),
-              Expanded(
-                child: Consumer<GameState>(
-                    builder: (context, game, _) => GridView.count(
-                          crossAxisCount: 2,
-                          children: game.game.players
-                              .map((p) => PlayerAvatar(player: p))
-                              .toList(),
-                        )),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 40, bottom: 10),
-                child: Button(
-                  type: ButtonTypes.accent,
-                  title: "Start Game",
-                  onPressed: startGame,
-                ),
-              )
-            ])));
+            child: PlayerLobby(
+              onStartGame: startGame,
+            )));
   }
 }
